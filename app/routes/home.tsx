70ae2657a98da745c7bd763610e212c3a75fbf5c
thakordixit567 <103193972+thakordixit567@users.noreply.github.com>
2025-08-19 14:35:46 +0530

    UPDATE!

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,22 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart Feedback For Your Dreame Job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+     
+     <Navbar/>
+
+    <section className="main-section">
+       <div className="page-heading">
+          <h1> Track Your Application & Resume Ratings </h1>
+          <h2> Review you submissons and check AI-Powered feedback. </h2>
+       </div>
+    </section>
+  </main>
 }
